15bd24f9db51f9c398af7d55302f00a10af6f2ca
Romi Qelibari <bajram.qelibari@googlemail.com>
2021-04-09 15:06:45 +0200

    Implemented the Soundex Algorithm

--- soundex.py
+++ soundex.py
@@ -1,4 +1,14 @@
 def soundex(word):
-    for letter in 'aeiouyhw':
-        word = word.replace(letter, '')
-    return word
+    result = []
+    mapping = {'aeiouywh': '', 'bfpv': '1', 'cgjkqsxz': '2', 'dt': '3', 'l': '4', 'mn': '5', 'r': '6'}
+    for index, letter in enumerate(word, 1):
+        for key in mapping.keys( ):
+            if letter in key:
+                result.append(mapping[key])
+    result[0] = word[:1]
+    if len(result) < 3:
+        result.append('0')
+    result = list(dict.fromkeys(result))
+    return ''.join(result)
+
+print(soundex('Qelibari'))
--- test_soundex.py
+++ test_soundex.py
@@ -1,15 +1,49 @@
 import unittest
 from soundex import soundex
 
+
 class SoundexTestCase(unittest.TestCase):
     def test_retainsFirstLetter(self):
         code = soundex('myword')
         self.assertEqual('m', code[0])
 
-    def test_removesRemainingVowels(self):
+    def test_removesVovels(self):
         code = soundex('maauoy')
         self.assertEqual('m', code)
+        code = soundex('aeiouwhy')
+        self.assertEqual('a', code)
 
+    def test_replacesConsonantsWith1(self):
+        code = soundex('mbpfvv')
+        self.assertEqual('m1',code)
+
+    def test_replacesConsonantsWith2(self):
+        code = soundex('cgjkqsxz')
+        self.assertEqual('c2',code)
+
+    def test_replacesConsonantsWith3(self):
+        code = soundex('mdtdtdt')
+        self.assertEqual('m3',code)
+
+    def test_replacesConsonantsWith4(self):
+        code = soundex('mllll')
+        self.assertEqual('m4',code)
+
+    def test_replacesConsonantsWith5(self):
+        code = soundex('mmnmnmn')
+        self.assertEqual('m5',code)
+
+    def test_replacesConsonantsWith6(self):
+        code = soundex('mrrr')
+        self.assertEqual('m6',code)
+
+    def test_fullAlgorithm(self):
+        code = soundex("Qelibari")
+        self.assertEqual('Q416', code)
+        code = soundex("Robert")
+        self.assertEqual('R163', code)
+        code = soundex("Rupert")
+        self.assertEqual('R163', code)
 
 if __name__ == '__main__':
     unittest.main( )
